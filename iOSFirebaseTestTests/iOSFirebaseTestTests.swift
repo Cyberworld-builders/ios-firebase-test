88c831bea3728f6d8b26d0150dc0dbf4bb0bc186
Jay Long <jaylong255@gmail.com>
2025-03-16 15:52:19 -0500

    moving xcode project into root

---
+++ iOSFirebaseTestTests/iOSFirebaseTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSFirebaseTestTests.swift
+//  iOSFirebaseTestTests
+//
+//  Created by Jay Long on 3/16/25.
+//
+
+import Testing
+@testable import iOSFirebaseTest
+
+struct iOSFirebaseTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
